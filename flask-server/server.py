063d8dee804fd4486fe461e09b7398b7026d9938
sakthi535 <ssakthi20030305@gmail.com>
2022-05-29 18:01:28 +0530

    Flask changes

--- flask-server/server.py
+++ flask-server/server.py
@@ -15,8 +15,8 @@ app = Flask(__name__)
 
 # sys.path.append('../Steam-game-recommendation-system')
 
-dataset = pickle.load(open('./Dataset/meta_games_dict.pkl','rb'))
-ml = pickle.load(open('./Dataset/confusion_matrix_con.pkl','rb'))
+dataset = pickle.load(open('./../Dataset/meta_games_dict.pkl','rb'))
+ml = pickle.load(open('./../Dataset/confusion_matrix_con.pkl','rb'))
 
 
 #Using ANNOY library, finding recommendation for user selection
@@ -30,28 +30,23 @@ def test_annoy_knn(index, Ntrees, Nsamples, method = "euclidean"):
 
     t.build(Ntrees)
 
-    return t.get_nns_by_item(index, 11)[1:]
+    return t.get_nns_by_item(index, 15)[1:]
 
 # Dictionary where each key represents app id of each game in dataframe and value represents the index positions in it 
 indexTable = {v: k for k, v in dataset['appid'].items()}
 
-
-def Similarity(movieId1, movieId2):
+# Cosine Similarity function to determine difference as an equivalent "distance" between two games
+def Similarity(game1, game2):
     # a = movies.iloc[movieId1]
     # b = movies.iloc[movieId2]
     
-    gameA =ml[movieId1]
-    gameB = ml[movieId2]
+    gameA =ml[game1]
+    gameB = ml[game2]
     
-    if(len(gameA) != len(gameB)):
-        print(movieId2)
-        print("HERRRRREEE")
-        return 100
-
     return spatial.distance.cosine(gameA, gameB)
 
 
-
+# Determining new list of games to recommend from previous recommendations and newly selected recommendation
 def selectNewGames(recentSelected, gamesRecommended, newRecommendations):
     
     res = []
@@ -65,15 +60,17 @@ def selectNewGames(recentSelected, gamesRecommended, newRecommendations):
         else:
             a.append(i)
     
+    # Similarity towards recently selected games
     b=[]
     for i in a:
         b.append(Similarity(recentSelected, i))
     
+    # Remaining games in sorted order acc to Similarity towards recently selected game
     res+=([x for i, x in sorted(zip(b, a))])
     return res[:10]
     
 
-
+# Connection to react server in front end
 @app.route('/recommend/', methods=['POST'])
 def members():
 
@@ -82,6 +79,7 @@ def members():
     res = [[0]]*len(userSelectedGames)
     gamesRecommended = []
 
+    # Picking games from start to recently selected game
     for i in range(len(userSelectedGames)-1,-1,-1):
         if i == len(userSelectedGames)-1:
             res[i] = test_annoy_knn(indexTable[userSelectedGames[i]], 10, 16845, 'angular')
@@ -89,23 +87,25 @@ def members():
             print("result : ", gamesRecommended)
 
         else:
+            # List of games similar to userSelectedGames[i]
             newRecommendations = test_annoy_knn(indexTable[userSelectedGames[i]], 10, 16845, 'angular')
+            
+            # Recommendation to user based on userSelectedGames[0:i] games selected by user
             gamesRecommended = selectNewGames(indexTable[userSelectedGames[i]], gamesRecommended, newRecommendations)
             print("new :" , newRecommendations)
             print("result : ", gamesRecommended)
-
-    # res = test_annoy_knn(indexTable[userSelectedGames[0]], 10, 16845, 'angular')
-
 
     gamesRecommendedAppid = []
 
     for i in gamesRecommended:
         gamesRecommendedAppid.append(dataset['appid'][i])
 
-    # print(gamesRecommended)
-    # print(gamesRecommendedAppid)
+    # Filtering games that are already selected by user
+    for i in userSelectedGames:
+        if i in gamesRecommendedAppid:
+            gamesRecommendedAppid.remove(i)
 
-    return {"list": gamesRecommendedAppid}
+    return {"list": gamesRecommendedAppid[:8]}
 
 if __name__ == "__main__":
     app.run(debug=True)
